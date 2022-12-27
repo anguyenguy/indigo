c9ede93f7af6ec89cd767d28d937b5328fd31a75
quannguyen <hdragon1501@gmail.com>
2022-12-28 01:12:16 +0700

    Update

--- lms/static/js/login-page.js
+++ lms/static/js/login-page.js
@@ -3,12 +3,19 @@ $(function () {
 
         var $button = $('#login-button-styled');
 
+        var $button_gg = $('#login-button-gg-oauth');
+
         $button.click(function(){
                 $('#login-email').val($('#login-email-styled').val());
                 $('#login-password').val($('#login-password-styled').val());
-                console.log('button 1 click');
                 $('.login-button').click(); 
                 return false;
+        });
+
+
+        $button_gg.click(function(){
+                console.log("on click")
+                $('.login-oa2-google-oauth2').click();
         });
 
 });
