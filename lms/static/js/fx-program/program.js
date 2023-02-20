cd51032288e21a7d8826c1a9811ba1cebcda9c7a
SangLM <sanglm@funix.edu.vn>
2023-02-20 08:45:56 +0700

    disable btn link into screen 576px

--- lms/static/js/fx-program/program.js
+++ lms/static/js/fx-program/program.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
                     <div class="card-deck">
                         <div class="card" >
                           <div class="position-relative">
-                            <div class="position-absolute top-0 start-0 p-2 ${courseData.language === 'en' ? 'bg-primary text-white' : 'bg-danger text-white'}" style="top: 15px;">
+                            <div class="position-absolute top-0 start-0 p-2 ${courseData.language === 'en' ? 'bg-warning text-white' : 'bg-primary text-white'}" style="top: 15px;">
                               <span>${courseData.language === 'en' ? 'Tiếng Anh' : 'Tiếng Việt'}</span>
                             </div>
                             <img src="${course_image_url}" class="card-img-top w-100 img-fluid" style="padding: 15px; border-radius: 20px" alt="Ảnh khóa học" />
